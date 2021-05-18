7914b694b24ec7f2f530b3ff16a8b4dcd6e5cbe4
Pawel W <pawiercioch@gmail.com>
2021-05-19 00:55:52 +0200

    Modify code to be simple OOP Created one class. Every used function put inside it.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from data_operations import *
 from visualization import *
-
+'''
 file_names=get_file_names("/Test_data")
 
 data=read_data("Test_data/",file_names)
@@ -12,3 +12,14 @@ time=create_time(data,10)
 calculate_RMS(data)
 
 plot_RMS(data)
+'''
+rawData=Data()
+rawData.get_file_names("/Test_data")
+rawData.read_data("Test_data/")
+rawData.create_time(rawData.data, 10)
+
+# rawData.raw_data_plots(rawData.data)
+
+rawData.calculate_RMS(rawData.data)
+
+rawData.plot_RMS(rawData.data)
